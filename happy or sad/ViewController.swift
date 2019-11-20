1db3d75725d43d9a65a851552304fd076b8f640a
aidan melville <amelville@lcs.on.ca>
2019-11-20 11:32:23 -0500

    Done all the connections from the view to the controller (Outlets + Actions Created)

--- happy or sad/ViewController.swift
+++ happy or sad/ViewController.swift
@@ -10,11 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //Mark: Propertys
+    //Make Outlets
+    
+    @IBOutlet weak var phraseInputField: UITextField!
+    
+    @IBOutlet weak var outputResults: UILabel!
+    
+    //Mark Methods
+    //Functions(Make things happen)
+    //Actions
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    //Analyze the text provided
+    
+    @IBAction func analyzeText(_ sender: Any) {
+        
+        
+        
+    }
+    
+    
 }
 
